b2f9e04b5e21ec2d8372c595917b55792228db95
bright <dymerinov@gmail.com>
2022-06-15 22:45:19 +0300

    finish

--- CianTest/UnitTest1.cs
+++ CianTest/UnitTest1.cs
@@ -10,9 +10,18 @@ namespace CianTest
 
         private readonly By signInButton = By.XPath("//span[text()='Войти']");
         private readonly By loginInputButton = By.XPath("//input[@name='username']");
-        private readonly By otherLoginButton = By.XPath("//span[text()='Другим способом']");
+        private readonly By otherLoginButton = By.XPath("//button[@data-name='SwitchToEmailAuthBtn']");
+        private readonly By continueButton = By.XPath("//button[@data-name='ContinueAuthBtn']");
+        private readonly By passButton = By.XPath("//input[@name='password']");
+        private readonly By userAvatar = By.XPath("//a[@data-name='UserAvatar']");
+        private readonly By actualId = By.XPath("//div[@class='_25d45facb5--user-name--oMFPn']");
 
-        private const string loginMail = "kiwik950@mail.ru";
+
+
+        private const string loginMail = "tester18000@mail.ru";
+        private const string cianpw = "cianpassw";
+        private const string myId = "ID 92178388";
+
 
         [SetUp]
         public void Setup()
@@ -33,6 +42,26 @@ namespace CianTest
 
             var login = driver.FindElement(loginInputButton);
             login.SendKeys(loginMail);
+
+            var continueBut = driver.FindElement(continueButton);
+            continueBut.Click();
+
+            Thread.Sleep(1500);
+            var pass = driver.FindElement(passButton);
+            pass.SendKeys(cianpw);
+            
+            var signInFin = driver.FindElement(continueButton);
+            signInFin.Click();
+
+            Thread.Sleep(1500);
+            var userAva = driver.FindElement(userAvatar);
+            userAva.Click();
+
+            var myActualId = driver.FindElement(actualId).Text;
+
+            Assert.AreEqual(myId, myActualId, "Login is wrong.");   
+
+
         }
 
         [TearDown]
